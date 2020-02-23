c0ae766ce52ecc4c9f49285884ffa52f8aa8f0b8
beans907 <kyou8274@gmail.com>
2020-02-24 01:28:03 +0900

    -프로젝트 생성 및 에셋추가

---
+++ Source/Town/TownGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TownGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TOWN_API ATownGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
